500ac06350326fb1293666667b87442fc1c6c682
Murielly-Nascimento <murielly.nascimento@ufu.br>
2023-03-29 15:13:13 -0300

    Seleção por torneio

--- main.c
+++ main.c
@@ -3,15 +3,15 @@
 #include <string.h>
 #include <time.h>
 
-const char alvo[] = "o tejo guarda grandes navios";
-const char alfabeto[] = "abcdefghijklmnopqrstuvxwz ";
+#define TAMANHO	29
+typedef struct{
+	int fitness;
+	char frase[TAMANHO];
+}POPULACAO;
 
-#define TAMANHO (sizeof(alvo) / sizeof(char))
-#define ESCOLHA (sizeof(alfabeto) - 1)
-
-#define MUTACAO 15
-#define COPIAS 300
-#define GERACOES 300
+#define MUTACAO	15
+#define GERACOES 30
+#define INDIVIDUOS 300
 
 int gerarNumAleatorio(int n)
 {
@@ -19,95 +19,110 @@ int gerarNumAleatorio(int n)
 	return r;
 }
 
-int grauDeDiferenca(const char *copia)
+int fitness(POPULACAO copia, const char *alvo)
 {
-	int i = 0, qtdDistintos = 0;
-	for (i = 0; i < TAMANHO; i++){
-		if(copia[i] != alvo[i])
+	int qtdDistintos = 0;
+
+	for (int i = 0; i < TAMANHO; i++){
+		if(copia.frase[i] != alvo[i])
 			qtdDistintos++;
 	}
 	return qtdDistintos;
 }
 
-int selecaoPorTorneio(char populacao[COPIAS][TAMANHO]){
+void mutacao(POPULACAO filho)
+{
+	const char alfabeto[] = "abcdefghijklmnopqrstuvxwz ";
 
-	int melhor=RAND_MAX, segundoMelhor=RAND_MAX;
-	int unfit = 0, pai = 0, mae = 0;
+	int r = gerarNumAleatorio(100);
+	int posicao = gerarNumAleatorio(TAMANHO-2);
 
-	for(int i = 2; i < COPIAS; i++){
-		unfit = grauDeDiferenca(populacao[i]);
-		if(unfit < melhor){
-			melhor = unfit;
-			pai = i;
-		}
-		else if(unfit < segundoMelhor){
-			segundoMelhor = unfit;
-			mae = i;
-		}
+	if(r <= MUTACAO)
+		filho.frase[posicao] = alfabeto[gerarNumAleatorio(28)];
+}
+
+POPULACAO recombinacaoUmPonto(POPULACAO pai, POPULACAO mae){
+	int pontoDeCrossover = gerarNumAleatorio(TAMANHO-2);
+	POPULACAO filho;
+
+	for(int i = 0; i < TAMANHO; i++){
+		if(i <= pontoDeCrossover)
+			filho.frase[i] = pai.frase[i];
+		else
+			filho.frase[i] = mae.frase[i];
+	}
+	return filho;
+}
+
+POPULACAO torneio(POPULACAO A, POPULACAO B){
+	if(A.fitness < B.fitness) return A;
+	else return B;
+}
+
+POPULACAO selecaoPorTorneio(POPULACAO populacao[INDIVIDUOS],const char* alvo){
+	POPULACAO novaPopulacao[INDIVIDUOS];
+	POPULACAO melhor;
+	melhor.fitness=RAND_MAX;
+
+	for(int i = 0; i < INDIVIDUOS; i++){
+		POPULACAO pai = torneio(populacao[gerarNumAleatorio(INDIVIDUOS-1)], populacao[gerarNumAleatorio(INDIVIDUOS-1)]);
+		POPULACAO mae = torneio(populacao[gerarNumAleatorio(INDIVIDUOS-1)], populacao[gerarNumAleatorio(INDIVIDUOS-1)]);
+		
+		POPULACAO filho = recombinacaoUmPonto(pai, mae);
+		mutacao(filho);
+		filho.fitness = fitness(filho,alvo);
+
+		if(filho.fitness < melhor.fitness) 
+			melhor = filho;
+		
+		novaPopulacao[i] = filho;
 	}
 
-	strcpy(populacao[0], populacao[pai]);
-	strcpy(populacao[1], populacao[mae]);
+	for(int i = 0; i < INDIVIDUOS; i++)
+		populacao[i] = novaPopulacao[i];
 
 	return melhor;
 }
 
-void mutacao(char *descendente)
+void inicializa(POPULACAO populacao[INDIVIDUOS], const char *alvo)
 {
-	int r = gerarNumAleatorio(100);
-	int posicao = gerarNumAleatorio(TAMANHO-2);
-	if(r <= MUTACAO)
-		descendente[posicao] = alfabeto[gerarNumAleatorio(ESCOLHA)];
-}
+	const char alfabeto[] = "abcdefghijklmnopqrstuvxwz ";
 
-void recombinacaoUmPonto(char *pai, char *mae, char *filho){
-	int pontoDeCrossover = TAMANHO/2;
-
-	for(int i = 0; i < TAMANHO; i++){
-		if(i <= pontoDeCrossover)
-			filho[i] = pai[i];
-		else
-			filho[i] = mae[i];
+	int i = 0, j = 0;
+	for (i = 0; i < INDIVIDUOS; i++){
+		for(j = 0; j < TAMANHO-1; j++){
+			if(j == 0 && i == 0){
+				srand(time(NULL));
+				int letra  = rand() % 25;
+				populacao[i].frase[j] = alfabeto[letra];
+			}
+			else 
+				populacao[i].frase[j] = alfabeto[gerarNumAleatorio(25)];
+		}
+		populacao[i].frase[j] = '\0';
+		populacao[i].fitness = fitness(populacao[i], alvo);
 	}
-}
-
-int inicializa(char *primeiroDescendente)
-{
-	int letraAleatoria = 0, i = 0;
-	srand(time(NULL));
-	letraAleatoria = rand() % ESCOLHA;
-
-	primeiroDescendente[0] = alfabeto[letraAleatoria];
-
-	for(i = 1; i < TAMANHO-1; i++)
-		primeiroDescendente[i] = alfabeto[gerarNumAleatorio(ESCOLHA)];
-
-	primeiroDescendente[i] = '\0';
-	
-	return letraAleatoria;
 }
 
 int main(void)
 {
-	int iteracao = 0;
-	char populacao[COPIAS][TAMANHO] = {'\0'};
-
-	// Inicializamos o pai e a mae
-	inicializa(populacao[0]);
-	strcpy(populacao[1],alfabeto);
+	const char alvo[] = "o tejo guarda grandes navios";
 	
+	int geracao = 0;
+	POPULACAO populacao[INDIVIDUOS];
+
+	inicializa(populacao, alvo);
+
 	do{
-		for (int i = 2; i < COPIAS; i++){
-			recombinacaoUmPonto(populacao[i-2], populacao[i-1], populacao[i]);
-			mutacao(populacao[i]);
-		}
+		POPULACAO melhor;
+		melhor.fitness=RAND_MAX;
 
-		int melhor = selecaoPorTorneio(populacao);
+		melhor = selecaoPorTorneio(populacao, alvo);
 
-		printf("Iteracao %d, pai pontos %d: %s\n", iteracao, melhor, populacao[0]);
-		iteracao++;
+		printf("Iteracao %d, pai pontos %d: %s\n", geracao, melhor.fitness, melhor.frase);
+		geracao++;
 
-	}while (iteracao <= GERACOES);
+	}while (geracao <= GERACOES);
 
 	return 0;
 }
