4899fa95e8a622df56da26f528123c689b480383
Murielly-Nascimento <murielly.nascimento@ufu.br>
2023-08-16 11:53:59 -0300

    Documentação da API do código main.c

--- main.c
+++ main.c
@@ -3,25 +3,34 @@
 #include <string.h>
 #include <time.h>
 #include <conio.h>
-#include <sys/time.h>
 
 #define LETRAS 55
 #define TAMANHO	117
-
 typedef struct{
 	int fitness;
 	char frase[TAMANHO];
 }POPULACAO;
 
-//Parâmetros do Algoritmo Evolutivo
+
 #define MUTACAO	15
 #define GERACOES 280
 #define INDIVIDUOS 400
 #define TORNEIO 3
 #define ELITISMO 10
 
-/*Escreve em um arquivo texto o tempo para execução
-  do programa e o fitness do melhor indivíduo obtido*/
+/* Função: escreveRelatorio
+
+   Escreve no arquivo relatorio.txt o tempo para execução
+   do programa e o fitness do melhor indivíduo obtido.
+
+	Parâmetros:
+		tempo - Tempo gasto para execução do programa.
+		fitness - Número de caracteres distintos da frase alvo 
+				  do indivíduo mais adaptado da População.
+	
+	Retorno: 
+		Nulo.
+*/
 void escreveRelatorio(double tempo, int fitness){
 	FILE *arq;
 	arq = fopen("relatorio.txt", "a");
@@ -36,14 +45,38 @@ void escreveRelatorio(double tempo, int fitness){
 	fclose(arq);
 }
 
+/* Função: gerarNumAleatorio
+
+   Gera um número aleatório no intervalo de 0 a N-1.
+
+	Parâmetros:
+		n - Intervalo.
+		
+	Retorno: 
+		Um número inteiro aleatório.
+	
+	Confira o seguinte link para mais informações sobre a função rand()
+		https://c-faq.com/lib/randrange.html
+*/
 int gerarNumAleatorio(int n)
 {
 	int r = rand() % n;
 	return r;
 }
 
-/*O fitness do indivíduo é calculado pelo número de caracteres distintos entre 
-a frase cópia e a alvo, quanto menor esse número mais adaptado o indivíduo é.*/
+/* Função: fitness
+
+    Calcula o fitness de um indivíduo baseado no número de caracteres
+	distintos entre a frase alvo e a cópia. Quanto menor o fitness
+	mais adaptado o indivíduo é.
+
+	Parâmetros:
+		copia - Indivíduo.
+		alvo - Sequência de caracteres da frase alvo
+		
+	Retorno: 
+		Um número inteiro que representa o fitness do indivíduo.
+*/
 int fitness(POPULACAO copia, const char *alvo)
 {
 	int qtdDistintos = 0;
@@ -55,7 +88,18 @@ int fitness(POPULACAO copia, const char *alvo)
 	return qtdDistintos;
 }
 
-/*A população é inicializada aleatóriamente e o seu fitness já é calculado*/
+/* Função: inicializa
+
+    Inicializa as frases da sequência população e calcula o fitness
+	de cada uma delas.
+
+	Parâmetros:
+		população - sequência do tipo POPULACAO de tamanho INDIVIDUOS.
+		alvo - Sequência de caracteres da frase alvo.
+		
+	Retorno: 
+		Nulo.
+*/
 void inicializa(POPULACAO populacao[INDIVIDUOS], const char *alvo)
 {
 	const char alfabeto[] = "ABCDEFGHIJKLMNOPQRSTUVXWZabcdefghijklmnopqrstuvxwz,\n.  ";
@@ -69,9 +113,18 @@ void inicializa(POPULACAO populacao[INDIVIDUOS], const char *alvo)
 	}
 }
 
-/*Considerando a taxa de mutação, se um número aleatório gerado for
-menor ou igual a ela, uma letra da frase cópia é alterada.*/
-POPULACAO mutacao(POPULACAO filho){ 
+/* Função: mutacao
+
+    Altera o gene (caracter) de uma frase filho (cópia).
+
+	Parâmetros:
+		filho - Um dos indivíduos da população.
+		alvo - Sequência de caracteres da frase alvo.
+		
+	Retorno: 
+		O indivíduo da população após ser mutado.
+*/
+POPULACAO mutacao(POPULACAO filho, const char *alvo){ 
 	POPULACAO individuo = filho;
 	const char alfabeto[] = "ABCDEFGHIJKLMNOPQRSTUVXWZabcdefghijklmnopqrstuvxwz,\n.  ";
 
@@ -79,11 +132,24 @@ POPULACAO mutacao(POPULACAO filho){
 	int posicao = gerarNumAleatorio(TAMANHO-2);
 
 	if(r <= MUTACAO)
-		individuo.frase[posicao] = alfabeto[gerarNumAleatorio(LETRAS)];
+		individuo.frase[posicao] = alvo[posicao];
+	//individuo.frase[posicao] = alfabeto[gerarNumAleatorio(LETRAS)];
 
 	return individuo;
 }
 
+/* Função: recombinacaoUniforme
+
+    Combina aleatóriamente os genes (caracteres) da frase pai e mãe
+	no filho.
+
+	Parâmetros:
+		pai - Um dos indivíduos da população.
+		mae - Um dos indivíduos da população.
+		
+	Retorno: 
+		O indivíduo filho resultante da combinação do pai e mãe.
+*/
 POPULACAO recombinacaoUniforme(POPULACAO pai, POPULACAO mae){
 	POPULACAO filho;
 
@@ -96,6 +162,18 @@ POPULACAO recombinacaoUniforme(POPULACAO pai, POPULACAO mae){
 	return filho;
 }
 
+/* Função: selecaoPorTorneio
+
+    Enquanto o torneio entre N indivíduos não terminar
+	seleciona dois a dois e determina qual deles
+	possui o menor fitness.
+
+	Parâmetros:
+		populacao - população de indivíduos (frases cópias).
+		
+	Retorno: 
+		O indivíduo com menor fitness ontido pelo torneio.
+*/
 POPULACAO selecaoPorTorneio(POPULACAO populacao[INDIVIDUOS]){
 	POPULACAO melhor;
 	melhor.fitness = -1;
@@ -109,6 +187,17 @@ POPULACAO selecaoPorTorneio(POPULACAO populacao[INDIVIDUOS]){
 	return melhor;
 }
 
+/* Função: shellSort
+
+    Ordena a população, com base no fitness, decrescente.
+
+	Parâmetros:
+		populacao - população de indivíduos (frases cópias).
+		n - distância de salto.
+		
+	Retorno: 
+		Nulo.
+*/
 void shellSort(POPULACAO populacao[INDIVIDUOS], int n) {
 	for (int intervalo = n/2; intervalo > 0; intervalo/= 2) {
 		for (int i = intervalo; i < n; i+= 1) {
@@ -122,9 +211,20 @@ void shellSort(POPULACAO populacao[INDIVIDUOS], int n) {
 	}
 }
 
-/*É calculado a quantidade de indivíduos (os melhores) que ocuparão
-as posições iniciais do vetor e não sofreram a ação dos agentes mutação 
-e recombinação na função reprodução. */
+/* Função: elitismo
+
+    Calcula o número de indivíduos da população que
+	não sofreram a ação dos operadores recombinação e mutação
+	na função reproducao. Em seguida chama a função de ordenação
+	shellSort, os indivíduos mais adaptados ocupam as posições iniciais.
+
+	Parâmetros:
+		populacao - População de indivíduos (frases cópias).
+		alvo - Sequência de caracteres da frase alvo.
+		
+	Retorno: 
+		Número de indivíduos selecionados.
+*/
 int elitismo(POPULACAO populacao[INDIVIDUOS],const char* alvo){
 	int selecionados = INDIVIDUOS*ELITISMO/100;
 	
@@ -134,9 +234,19 @@ int elitismo(POPULACAO populacao[INDIVIDUOS],const char* alvo){
 	return selecionados;
 }
 
-/*Esta função é responsável por coordenar a reprodução dos indivíduos.
-Nela as demais funções (elitismo, torneio, mutação, recombinação) são 
-aplicadas na população.*/
+/* Função: reproducao
+
+	Aplica as funções de elitismo, torneio, recombinação, mutação,
+	retornando uma nova população.
+
+	Parâmetros:
+		populacao - População de indivíduos (frases cópias).
+		alvo - Sequência de caracteres da frase alvo.
+		geracoes - Número de gerações executadas.
+
+	Retorno: 
+		Melhor indivíduo da população.
+*/
 POPULACAO reproducao(POPULACAO populacao[INDIVIDUOS],const char* alvo, int geracoes)
 {
 	POPULACAO novaPopulacao[INDIVIDUOS], melhor, pai, mae, filho;
@@ -153,7 +263,7 @@ POPULACAO reproducao(POPULACAO populacao[INDIVIDUOS],const char* alvo, int gerac
 		mae = selecaoPorTorneio(populacao);
 
 		filho = recombinacaoUniforme(pai, mae);
-		filho = mutacao(filho);
+		filho = mutacao(filho, alvo);
 		filho.fitness = fitness(filho,alvo);
 
 		if(filho.fitness < melhor.fitness) 
@@ -168,6 +278,11 @@ POPULACAO reproducao(POPULACAO populacao[INDIVIDUOS],const char* alvo, int gerac
 	return melhor;
 }
 
+/* Função: main
+
+	Implementa o Algoritmo Evolutivo para o problema: dado uma frase alvo 
+	reproduza-a a partir de uma população de frases cópias geradas aleatóriamente.
+*/
 int main(void)
 {
 	clock_t inicio, fim;
